9cfb6b25d054e7ac4532fd11f901d3f853b7fcf6
aliemreoz <alioz@std.sehir.edu.tr>
2017-12-12 20:23:42 +0300

    On Top and Process Killer

---
+++ Security/onTop.py
@@ -0,0 +1,22 @@
+import win32gui
+import win32con
+import pyHook
+
+def OnKeyboardEvent(event):
+
+    if event.Key.lower() in ["lwin","escape","f4","lmenu","rmenu"]:
+        return False
+    else:
+        return True
+
+def on():
+
+    while True:
+        hook_manager = pyHook.HookManager()
+        hook_manager.KeyDown = OnKeyboardEvent
+        hook_manager.HookKeyboard()
+        window = win32gui.FindWindow(None, "Seas")
+        win32gui.ShowWindow(window, win32con.SW_MAXIMIZE)
+        win32gui.SetWindowPos(window, win32con.HWND_TOPMOST, 0, 0, 0, 0, (win32con.SWP_NOMOVE | win32con.SWP_NOSIZE))
+
+on()
