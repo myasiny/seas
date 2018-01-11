03c9ff6352d5ed1279fc20fb16e751676d7100e9
aliemreoz <alioz@std.sehir.edu.tr>
2018-01-11 21:10:41 +0300

    Basic Compiler

---
+++ Functionality/excelToCsv.py
@@ -0,0 +1,17 @@
+import xlrd
+import unicodecsv
+
+def xls2csv(xls_filename,output_filename):
+
+    wb = xlrd.open_workbook(xls_filename)
+    sh = wb.sheet_by_index(0)
+
+    fh = open(output_filename,"wb")
+    csv_out = unicodecsv.writer(fh, encoding='utf-8')
+
+    for row_number in xrange (sh.nrows):
+        csv_out.writerow(sh.row_values(row_number))
+
+    fh.close()
+    return output_filename
+
