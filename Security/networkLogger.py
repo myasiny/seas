f9375788be70d8d77f9b49bd82057f46cf058472
aliemreoz <alioz@std.sehir.edu.tr>
2018-03-15 22:21:32 +0300

    network logger

---
+++ Security/networkLogger.py
@@ -0,0 +1,26 @@
+import time
+import psutil
+
+def main():
+    old_value = 0
+
+    while True:
+        new_value = psutil.net_io_counters().bytes_sent + psutil.net_io_counters().bytes_recv
+
+        if old_value:
+            send_stat(new_value - old_value)
+
+        old_value = new_value
+
+        time.sleep(3)
+
+def convert_to_gbit(value):
+    return value/1024./1024./1024.*8
+
+def convert_to_mbit(value):
+    return convert_to_gbit(value)*1024
+
+def send_stat(value):
+    print ("%0.3f" % convert_to_mbit(value))
+
+main()
