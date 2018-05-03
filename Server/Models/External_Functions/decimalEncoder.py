0c5b09e97172a9dfd6c964c7ec0f3b07a69dcf25
myasiny <myasiny@gmail.com>
2018-05-03 21:21:13 +0300

    Frankfurt-Branch On Air

--- Server/Models/External_Functions/decimalEncoder.py
+++
@@ -1,8 +0,0 @@
-import json
-from decimal import Decimal as D
-
-class DecimalEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, D):
-            return float(obj)
-        return json.JSONEncoder.default(self, obj)
