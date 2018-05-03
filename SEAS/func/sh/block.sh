cfec78ab2e724dc1b8160a1a321116f745f0b62b
myasiny <myasiny@gmail.com>
2018-05-03 21:13:31 +0300

    Frankfurt-Branch Initialization

--- SEAS/func/sh/block.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-xmodmap -e 'keycode 64='
-xmodmap -e 'keycode 133='
-xmodmap -e 'keycode 135='
--- SEAS/func/sh/restore.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-xmodmap -e 'keycode 64=Alt_L Meta_L Alt_L Meta_L'
-xmodmap -e 'keycode 133=Super_L NoSymbol Super_L'
-xmodmap -e 'keycode 135=Menu NoSymbol Menu'
